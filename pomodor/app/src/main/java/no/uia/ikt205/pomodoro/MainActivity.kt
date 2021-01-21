34d5482e3ed5f137e8e8deb8d348c79c1ee4e417
Maris Uzuls <marisu18@uia.no>
2021-01-21 22:07:17 +0100

    buttons and bugfix

--- pomodor/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
+++ pomodor/app/src/main/java/no/uia/ikt205/pomodoro/MainActivity.kt
@@ -9,28 +9,36 @@ import android.widget.TextView
 import android.widget.Toast
 import no.uia.ikt205.pomodoro.util.millisecondsToDescriptiveTime
 
+lateinit var timer: CountDownTimer
+var timerOn = 0;
+
 class MainActivity : AppCompatActivity() {
 
-    lateinit var timer:CountDownTimer
+
     lateinit var startButton:Button
+
+    lateinit var b1:Button
+    lateinit var b2:Button
+    lateinit var b3:Button
+    lateinit var b4:Button
+
     lateinit var coutdownDisplay:TextView
 
-    val timeToCountDownInMs = 5000L
+    var timeToCountDownInMs = 0L
     val timeTicks = 1000L
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-       startButton = findViewById<Button>(R.id.startCountdownButton)
-       startButton.setOnClickListener(){
-           startCountDown(it)
-       }
-       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+    fun checker(){
 
+        if (timerOn==1){
+            timer.cancel()
+            timerOn=0;
+        }
     }
 
+
     fun startCountDown(v: View){
+
 
         timer = object : CountDownTimer(timeToCountDownInMs,timeTicks) {
             override fun onFinish() {
@@ -38,12 +46,61 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onTick(millisUntilFinished: Long) {
-               updateCountDownDisplay(millisUntilFinished)
+
+                updateCountDownDisplay(millisUntilFinished)
             }
+
         }
 
+        timerOn=1
         timer.start()
+
     }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       startButton = findViewById<Button>(R.id.startCountdownButton)
+
+        b1 = findViewById<Button>(R.id.time1)
+        b2 = findViewById<Button>(R.id.time2)
+        b3 = findViewById<Button>(R.id.time3)
+        b4 = findViewById<Button>(R.id.time4)
+
+        b1.setOnClickListener(){
+            checker()
+            timeToCountDownInMs = 1800000L
+            updateCountDownDisplay(timeToCountDownInMs)
+        }
+        b2.setOnClickListener(){
+            checker()
+            timeToCountDownInMs = 3600000L
+            updateCountDownDisplay(timeToCountDownInMs)
+        }
+        b3.setOnClickListener(){
+            checker()
+            timeToCountDownInMs = 5400000L
+            updateCountDownDisplay(timeToCountDownInMs)
+        }
+        b4.setOnClickListener(){
+            checker()
+            timeToCountDownInMs = 7200000L
+            updateCountDownDisplay(timeToCountDownInMs)
+        }
+
+       startButton.setOnClickListener(){
+
+           startCountDown(it)
+       }
+
+       coutdownDisplay = findViewById<TextView>(R.id.countDownView)
+
+    }
+
+
+
 
     fun updateCountDownDisplay(timeInMs:Long){
         coutdownDisplay.text = millisecondsToDescriptiveTime(timeInMs)
